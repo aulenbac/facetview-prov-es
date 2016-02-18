f9e2abeda7ec00579b7a21c69b91742401495623
Gerald Manipon <geraldjohn.m.manipon@jpl.nasa.gov>
2016-02-18 15:48:09 -0800

    point to new EC2 instance

--- fv_prov_es/settings.py
+++ fv_prov_es/settings.py
@@ -1,6 +1,6 @@
 class Config(object):
     SECRET_KEY = 'secret key'
-    ES_URL = 'http://128.149.61.135:9200' # default port is 9200
+    ES_URL = 'http://128.149.63.56:9200' # default port is 9200
 
     # for PROVES app
     PROVES_ES_PREFIX = 'prov_es'
